7e40959855582bde6f1e53b0995caf8b6083a868
Adrian DC <radian.dc@gmail.com>
2016-11-16 14:09:45 +0100

    huashan: aosp: Define AOSP related properties
     * Prevent Google SetupWizard from rotating
     * Show storage manager settings
    
    Change-Id: Iecff14c96e11feb22a5101437a9e2021b83398a4

--- product/aosp.mk
+++ product/aosp.mk
@@ -10,3 +10,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/install/backuptool.functions:install/bin/backuptool.functions \
     $(LOCAL_PATH)/install/backuptool.sh:install/bin/backuptool.sh
+
+# AOSP properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.setupwizard.rotation_locked=true \
+    ro.storage_manager.enabled=true
