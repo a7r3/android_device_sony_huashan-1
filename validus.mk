98bd5a01e38e0be7b53c9c476061844e15d4bc48
Arvindraj <arvindultimate7352@gmail.com>
2017-02-25 21:29:32 +0530

    GZRify
    * Add ROM Specific makefiles upon this Device Tree, coz #LazyLife
    	These makefiles inherit from main 'lineage.mk' makefile
    	And contain only ROM Vendor Specific stuff
    	and an Important ROM<->Device Identifier 'PRODUCT_NAME'
    
    * Add maintainer string for GZR Validus
    
    Signed-off-by: Arvindraj <arvindultimate7352@gmail.com>

--- validus.mk
+++ validus.mk
@@ -15,22 +15,17 @@
 # limitations under the License.
 #
 
-# Inherit common Lineage stuff
-# $(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit from common makefile
+$(call inherit-product, device/sony/huashan/lineage.mk)
 
-# Inherit device configurations
-$(call inherit-product, device/sony/huashan/device.mk)
+# Inherit Validus stuff
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
-# Device display
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
+# telephony-ext stuff
+$(call inherit-product, vendor/validus/config/caf_fw.mk)
+
+# Maintainer Prop
+PRODUCT_BUILD_PROP_OVERRIDES += DEVICE_MAINTAINERS="Arvindraj Thangaraj (Arvind7352)"
 
 # Device identifications
-PRODUCT_DEVICE := huashan
-PRODUCT_NAME := lineage_huashan
-PRODUCT_BRAND := Sony
-PRODUCT_MANUFACTURER := Sony
-PRODUCT_MODEL := Xperia SP
-
-# Build fingerprints
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5303 BUILD_FINGERPRINT=Sony/C5303/C5303:4.3/12.1.A.1.207/Nvt_nw:user/release-keys PRIVATE_BUILD_DESC="C5303-user 4.3 JB-MR2-VISKAN-140318-1014 227 test-keys"
+PRODUCT_NAME := validus_huashan
